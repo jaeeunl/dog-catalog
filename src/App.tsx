d0837e5d4e5c287cba1fd8e0fccbb4ca4a851969
Jaeeun Lee <jaeeun.studio@gmail.com>
2022-04-25 00:45:05 -0400

    Styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Flex } from '@adobe/react-spectrum'
+import { Flex, View } from '@adobe/react-spectrum'
 import { List } from './List';
 import { Compare } from './Compare';
 import { CompareProvider } from './Compare/CompareProvider';
@@ -9,17 +9,19 @@ const App = () => {
 
   return (
       <CompareProvider>
-        <Flex width='960px' direction='column' gap='size-150' marginX='auto'>
-          {error 
-            ? <p>Sorry, there was an error loading the data.</p>
-            : (
-              <>
-                <Compare/>
-                <List breeds={breeds}/>
-              </>
-            ) }        
+        <View paddingX='size-200'>
+          <Flex width='960px' height='size-6000' direction='column' gap='size-150' marginX='auto' marginY='size-500'>
+            {error 
+              ? <p>Sorry, there was an error loading the data.</p>
+              : (
+                <>
+                  <Compare/>
+                  <List breeds={breeds}/>
+                </>
+              ) }        
 
-        </Flex>
+          </Flex>
+        </View>
       </CompareProvider>
   );
 }
